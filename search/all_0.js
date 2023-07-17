2815bc7e984d07acb3afa978fc4031ad4e36a98f
zhuker <zhukov.alex@gmail.com>
2023-07-17 04:51:35 +0000

    Deploying to gh-pages from @ zhuker/amazon-kinesis-video-streams-webrtc-sdk-c@b820e2ea3d28c670da2fbc789face742cb949ef1 🚀

--- search/all_0.js
+++ search/all_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['introduction_2emd_0',['Introduction.md',['../Introduction_8md.html',1,'']]],
-  ['introduction_1',['Introduction',['../md_Introduction.html',1,'']]]
+  ['introduction_0',['Introduction',['../md_Introduction.html',1,'']]],
+  ['introduction_2emd_1',['Introduction.md',['../Introduction_8md.html',1,'']]]
 ];
