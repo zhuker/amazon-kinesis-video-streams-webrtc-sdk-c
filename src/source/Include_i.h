5569c6aaed273a537ab2088481fea47281c74d48
Alex Zhukov <azhukov@roblox.com>
2025-06-01 00:08:57 -0700

    no locks

--- src/source/Include_i.h
+++ src/source/Include_i.h
@@ -71,6 +71,11 @@ extern "C" {
 #include <poll.h>
 #endif
 #endif
+#define MUTEX_CREATE(x) 42
+#define MUTEX_LOCK(x)
+#define MUTEX_UNLOCK(x)
+#define MUTEX_TRYLOCK(x) TRUE
+#define MUTEX_FREE(x)
 
 // Max uFrag and uPwd length as documented in https://tools.ietf.org/html/rfc5245#section-15.4
 #define ICE_MAX_UFRAG_LEN 256
