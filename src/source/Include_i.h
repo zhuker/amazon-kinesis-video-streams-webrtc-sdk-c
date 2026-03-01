6a3f99aa927ac40c58e860fd5f7b68ff5e344f2c
Alex Zhukov <azhukov@roblox.com>
2026-02-28 19:32:29 -0800

    removed Pacer.h

--- src/source/Include_i.h
+++ src/source/Include_i.h
@@ -177,7 +177,6 @@ STATUS generateJSONSafeString(PCHAR, UINT32);
 #include "PeerConnection/Rtp.h"
 #include "PeerConnection/Rtcp.h"
 #include "PeerConnection/Gcc_i.h"
-#include "PeerConnection/Pacer.h"
 #include "PeerConnection/DataChannel.h"
 #include "Rtp/Codecs/RtpVP8Payloader.h"
 #include "Rtp/Codecs/RtpH264Payloader.h"
