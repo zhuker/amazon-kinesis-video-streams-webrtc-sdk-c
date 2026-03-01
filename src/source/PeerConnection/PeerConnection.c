a9ffed33be21d0a672df200ede6ab3ece8a7f599
Alex Zhukov <zhukov.alex@gmail.com>
2026-03-01 13:58:12 -0800

    potential null ptr dereference fix (#21)

--- src/source/PeerConnection/PeerConnection.c
+++ src/source/PeerConnection/PeerConnection.c
@@ -1445,7 +1445,7 @@ UINT32 parseExtId(PCHAR extmapValue)
 {
     ENTERS();
     UINT32 extid = 0;
-    if (extmapValue == NULL && STRCHR(extmapValue, ' ') == NULL) {
+    if (extmapValue == NULL || STRCHR(extmapValue, ' ') == NULL) {
         LEAVES();
         return 0;
     }
